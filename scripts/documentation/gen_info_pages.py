03a9523b05a3cc715896bd37804c011303240373
Markus Binsteiner <markus@frkl.io>
2022-04-16 14:04:26 +0200

    refactor: move kiara.kiara -> kiara.context

--- scripts/documentation/gen_info_pages.py
+++ scripts/documentation/gen_info_pages.py
@@ -5,8 +5,8 @@
 
 import builtins
 
+from kiara.context import Kiara, KiaraContextInfo
 from kiara.doc.gen_info_pages import generate_detail_pages
-from kiara.kiara import Kiara, KiaraContextInfo
 
 pkg_name = "kiara_plugin.tabular"
 
